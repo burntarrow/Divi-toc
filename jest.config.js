3cf37bc386c7b4ea5824d624b9df0dc936f99b10
burntarrow <65312004+burntarrow@users.noreply.github.com>
2025-11-27 13:36:38 -0600

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,21 @@
+/** @type {import('jest').Config} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  roots: ['<rootDir>/src/components/table-of-contents-module/__tests__'],
-  moduleFileExtensions: ['ts', 'tsx', 'js'],
+
+  // Where your tests live
+  roots: ['<rootDir>/src'],
+
+  // Transform TS/TSX with ts-jest
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+
+  // Tell Jest what to do with non-JS imports like SCSS
+  moduleNameMapper: {
+    '\\.(css|scss)$': '<rootDir>/jest.styleMock.js',
+  },
+
+  // Optional: less noisy stack traces
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
